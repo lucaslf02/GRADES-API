137d521ce931915582755fc94cea02c0c9eda16b
lucaslf02 <lucas_llf2010@hotmail.com>
2020-07-11 15:52:31 -0300

    ajuste cors

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://polar-journey-35170.herokuapp.com/',
   })
 );
 
